e15ee9a7330d317f28354f662341b84fe50527bf
Lucas Støjko Andersen <93032255+stojl@users.noreply.github.com>
2025-11-26 13:03:38 +0100

    Update probability_callbacks.py
    Reorganize code into static methods

--- probability_callbacks.py
+++ probability_callbacks.py
@@ -2,84 +2,80 @@ import jax
 import jax.numpy as jnp
 
 
-@jax.jit
-def prob_callback_none(p: jnp.ndarray, p_point: jnp.ndarray) -> None:
-    return None
+class ProbabilityCallbacks:
 
+    @staticmethod
+    @jax.jit
+    def empty(p: jnp.ndarray, p_point: jnp.ndarray) -> None:
+        return None
 
-@jax.jit
-def prob_callback_default(
-    p: jnp.ndarray, p_point: jnp.ndarray
-) -> tuple[jnp.ndarray, jnp.ndarray]:
-    return p, p_point
+    @staticmethod
+    @jax.jit
+    def default(
+        p: jnp.ndarray, p_point: jnp.ndarray
+    ) -> tuple[jnp.ndarray, jnp.ndarray]:
+        return p, p_point
 
+    @staticmethod
+    @jax.jit
+    def no_duration(
+        p: jnp.ndarray, p_point: jnp.ndarray
+    ) -> tuple[jnp.ndarray, jnp.ndarray]:
+        return p[..., -1], p_point[..., -1]
 
-@jax.jit
-def prob_callback_no_duration(
-    p: jnp.ndarray, p_point: jnp.ndarray
-) -> tuple[jnp.ndarray, jnp.ndarray]:
-    return p[..., -1], p_point[..., -1]
+    @staticmethod
+    @jax.jit
+    def collapse_point(p: jnp.ndarray, p_point: jnp.ndarray) -> jnp.ndarray:
+        p_with_point = p[..., 0, :] + jnp.expand_dim(p_point, axis=1)
+        p = p.at[..., 0, :].set(p_with_point)
+        return p
 
+    @staticmethod
+    @jax.jit
+    def collapse_point_no_duration(p: jnp.ndarray, p_point: jnp.ndarray) -> jnp.ndarray:
+        p = ProbabilityCallbacks.collapse_point(p, p_point)
+        return p[..., -1]
 
-@jax.jit
-def prob_callback_collapse_point(p: jnp.ndarray, p_point: jnp.ndarray) -> jnp.ndarray:
-    p_with_point = p[..., 0, :] + jnp.expand_dim(p_point, axis=1)
-    p = p.at[..., 0, :].set(p_with_point)
-    return p
+    @staticmethod
+    @jax.jit
+    def point_only(p: jnp.ndarray, p_point: jnp.ndarray) -> jnp.ndarray:
+        return p_point
 
+    @staticmethod
+    @jax.jit
+    def point_only_no_duration(p: jnp.ndarray, p_point: jnp.ndarray) -> jnp.ndarray:
+        return p_point[..., -1]
 
-@jax.jit
-def prob_callback_collapse_point_no_duration(
-    p: jnp.ndarray, p_point: jnp.ndarray
-) -> jnp.ndarray:
-    p = prob_callback_collapse_point(p, p_point)
-    return p[..., -1]
+    @staticmethod
+    @jax.jit
+    def no_point(p: jnp.ndarray, p_point: jnp.ndarray) -> jnp.ndarray:
+        return p
 
+    @staticmethod
+    @jax.jit
+    def no_point_no_duration(p: jnp.ndarray, p_point: jnp.ndarray) -> jnp.ndarray:
+        return p[..., -1]
 
-@jax.jit
-def prob_callback_point_only(p: jnp.ndarray, p_point: jnp.ndarray) -> jnp.ndarray:
-    return p_point
-
-
-@jax.jit
-def prob_callback_point_only_no_duration(
-    p: jnp.ndarray, p_point: jnp.ndarray
-) -> jnp.ndarray:
-    return p_point[..., -1]
-
-
-@jax.jit
-def prob_callback_no_point(p: jnp.ndarray, p_point: jnp.ndarray) -> jnp.ndarray:
-    return p
-
-
-@jax.jit
-def prob_callback_no_point_no_duration(
-    p: jnp.ndarray, p_point: jnp.ndarray
-) -> jnp.ndarray:
-    return p[..., -1]
-
-
-@jax.jit
-def get_probability_callback_from_str(str: str):
-    match str:
-        case "default":
-            return prob_callback_default
-        case "none":
-            return prob_callback_none
-        case "no_duration":
-            return prob_callback_no_duration
-        case "collapse_point":
-            return prob_callback_collapse_point
-        case "collapse_point_no_duration":
-            return prob_callback_collapse_point_no_duration
-        case "point_only":
-            return prob_callback_point_only
-        case "point_only_no_duration":
-            return prob_callback_point_only_no_duration
-        case "no_point":
-            return prob_callback_no_point
-        case "no_point_no_duration":
-            return prob_callback_no_point_no_duration
-        case _:
-            return prob_callback_default
+    @staticmethod
+    def get_probability_callback_from_str(str: str):
+        match str:
+            case "default":
+                return ProbabilityCallbacks.default
+            case "none":
+                return ProbabilityCallbacks.empty
+            case "no_duration":
+                return ProbabilityCallbacks.no_duration
+            case "collapse_point":
+                return ProbabilityCallbacks.collapse_point
+            case "collapse_point_no_duration":
+                return ProbabilityCallbacks.collapse_point_no_duration
+            case "point_only":
+                return ProbabilityCallbacks.point_only
+            case "point_only_no_duration":
+                return ProbabilityCallbacks.point_only_no_duration
+            case "no_point":
+                return ProbabilityCallbacks.no_point
+            case "no_point_no_duration":
+                return ProbabilityCallbacks.no_point_no_duration
+            case _:
+                return ProbabilityCallbacks.default
